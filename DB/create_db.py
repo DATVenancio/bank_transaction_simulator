e6acd5e8c6671171a6620db1fd1ae6455a1f1640
DATVenancio <danielvenancioif@gmail.com>
2024-01-29 16:19:57 +0100

    UML

---
+++ DB/create_db.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="daniel",
+    passwd="daniel"
+)
+db_cursor = db.cursor()
+db_cursor.execute("CREATE DATABASE bank_system")
